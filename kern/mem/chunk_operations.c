83e3f0bda916d3cf3d75d3739982cf505b3f727f
MohamedamaD <mohamed.amad24483@gmail.com>
2022-11-24 14:19:19 +0200

    ms-2 \task2

--- kern/mem/chunk_operations.c
+++ kern/mem/chunk_operations.c
@@ -25,7 +25,27 @@ int cut_paste_pages(uint32* page_directory, uint32 source_va, uint32 dest_va, ui
 {
 	//TODO: [PROJECT MS2] [CHUNK OPERATIONS] cut_paste_pages
 	// Write your code here, remove the panic and write your code
-	panic("cut_paste_pages() is not implemented yet...!!");
+	uint32 start_src = ROUNDDOWN(source_va, PAGE_SIZE);
+	uint32 start_dest = ROUNDDOWN(dest_va, PAGE_SIZE);
+	uint32 end_src = ROUNDUP(source_va + (num_of_pages*PAGE_SIZE) , PAGE_SIZE);
+	uint32 end_dest = ROUNDUP(dest_va + (num_of_pages*PAGE_SIZE) ,PAGE_SIZE);
+	for (uint32 i = start_dest; i < end_dest; i+=PAGE_SIZE)
+	{
+		uint32* dest_ptr_page_table = NULL;
+		struct FrameInfo *dest_ptr_Frame_Info = get_frame_info(page_directory, i, &dest_ptr_page_table);
+		if (dest_ptr_Frame_Info != NULL)
+			return -1;
+	}
+
+	for (uint32 i = start_src, j = start_dest; i < end_src; i+=PAGE_SIZE,j+=PAGE_SIZE)
+	{
+		uint32* src_ptr_page_table = NULL;
+		struct FrameInfo *src_ptr_Frame_Info = get_frame_info(page_directory,i,&src_ptr_page_table);
+		uint32 perms = pt_get_page_permissions(page_directory, i);
+		int ret = map_frame(page_directory, src_ptr_Frame_Info, j, perms);
+		unmap_frame(page_directory, i);
+	}
+	return 0;
 }
 
 //===============================
@@ -38,7 +58,56 @@ int copy_paste_chunk(uint32* page_directory, uint32 source_va, uint32 dest_va, u
 {
 	//TODO: [PROJECT MS2] [CHUNK OPERATIONS] copy_paste_chunk
 	// Write your code here, remove the panic and write your code
-	panic("copy_paste_chunk() is not implemented yet...!!");
+	uint32 srcstrt = ROUNDDOWN(source_va, PAGE_SIZE);
+	uint32 srcend = ROUNDUP(source_va+size, PAGE_SIZE);
+	uint32 desstrt = ROUNDDOWN(dest_va, PAGE_SIZE);
+	uint32 desend = ROUNDUP(dest_va+size, PAGE_SIZE);
+	for (uint32 i = desstrt; i < desend; i += PAGE_SIZE)
+	{
+		uint32 *destable;
+		struct FrameInfo *desframe = get_frame_info(page_directory, i, &destable);
+		if (desframe != NULL)
+		{
+			uint32 desentry = destable[PTX(i)], write = desentry & PERM_WRITEABLE;
+			if (write == 0)
+				return -1;
+		}
+		if (destable == NULL)
+			create_page_table(page_directory, i);
+	}
+
+	// Biggest for loop (handle creating page table + allocating frames)
+	for (uint32 i = srcstrt, j = desstrt; i < srcend && j < desend; i += PAGE_SIZE, j += PAGE_SIZE)
+	{
+		uint32 *srctable, *destable;
+		struct FrameInfo *srcframe = get_frame_info(page_directory, i, &srctable),
+						 *desframe = get_frame_info(page_directory, j, &destable);
+		uint32 allperms = pt_get_page_permissions(page_directory, i);			// you get the user permission with the rest of them
+		if (desframe == NULL && srcframe != NULL)
+		{
+			struct FrameInfo *newframe;
+			int ret = allocate_frame(&newframe);
+			if (ret != 0)		// To handle not enough memory
+				return -1;
+			else
+			{
+				map_frame(page_directory, newframe, j, allperms);
+				pt_set_page_permissions(page_directory, j, PERM_WRITEABLE, 0);
+			}
+		}
+		else{
+			pt_set_page_permissions(page_directory, j, allperms, 0);}
+	}
+
+	// Loop for copying the content
+	for(uint32 i = source_va, j = dest_va; i < source_va+size && j < dest_va+size; i++, j++)
+	{
+		unsigned char * src = (unsigned char *)(i);
+		unsigned char * des = (unsigned char *)(j);
+		*des = *src;
+	}
+
+	return 0;
 }
 
 //===============================
@@ -50,7 +119,38 @@ int share_chunk(uint32* page_directory, uint32 source_va,uint32 dest_va, uint32 
 {
 	//TODO: [PROJECT MS2] [CHUNK OPERATIONS] share_chunk
 	// Write your code here, remove the panic and write your code
-	panic("share_chunk() is not implemented yet...!!");
+	uint32 s = ROUNDDOWN(source_va, PAGE_SIZE );
+	uint32 d = ROUNDDOWN(dest_va, PAGE_SIZE );
+	uint32 end = ROUNDUP(dest_va+size, PAGE_SIZE) ;
+	for(uint32 i = d ; i < end  ; i += PAGE_SIZE){
+			uint32* ptr_page_table2 = NULL;
+
+			struct FrameInfo *ptr_Frame_Info2 = get_frame_info(page_directory,d,&ptr_page_table2);
+			if(ptr_Frame_Info2 != NULL){
+				return -1 ;
+			}
+			if(ptr_page_table2 == NULL)
+				create_page_table(page_directory, i);
+	}
+	for(uint32 i = d ; i < end  ; i += PAGE_SIZE ){
+
+			uint32* ptr_page_table1 = NULL;
+			uint32* ptr_page_table2 = NULL;
+			// int ret = get_page_table(page_directory, d ,&ptr_page_table2 );
+			struct FrameInfo *ptr_Frame_Info1 = get_frame_info(page_directory,s,&ptr_page_table1);
+			struct FrameInfo *ptr_Frame_Info2 = get_frame_info(page_directory,d,&ptr_page_table2);
+			if(ptr_Frame_Info2 != NULL){
+				return -1 ;
+			}else{
+
+			//   uint32 prm = pt_get_page_permissions( page_directory, s );
+
+			   map_frame(page_directory , ptr_Frame_Info1 , d , perms );
+			}
+			s = s + PAGE_SIZE ;
+			d = d + PAGE_SIZE ;
+	}
+	return 0;
 }
 
 //===============================
@@ -62,7 +162,37 @@ int allocate_chunk(uint32* page_directory, uint32 va, uint32 size, uint32 perms)
 {
 	//TODO: [PROJECT MS2] [CHUNK OPERATIONS] allocate_chunk
 	// Write your code here, remove the panic and write your code
-	panic("allocate_chunk() is not implemented yet...!!");
+	uint32 v = ROUNDDOWN( va, PAGE_SIZE );
+	uint32 end = ROUNDUP( va + size , PAGE_SIZE );
+	for(uint32 i = v ; i < end  ; i += PAGE_SIZE ){
+		uint32* ptr_page_table = NULL;
+		struct FrameInfo *ptr_Frame_Info = get_frame_info(page_directory,i,&ptr_page_table);
+		if(ptr_Frame_Info != NULL )
+			return -1 ;
+		int ret = get_page_table(page_directory,v,&ptr_page_table );
+		if(ptr_page_table == NULL)
+			create_page_table(page_directory, i);
+//		v = v + PAGE_SIZE;
+	}
+
+		v = ROUNDDOWN( va, PAGE_SIZE );
+		end = ROUNDUP( va + size , PAGE_SIZE );
+	for(uint32 i = v ; i < end  ; i += PAGE_SIZE ){
+		uint32* ptr_page_table = NULL;
+		struct FrameInfo *ptr_Frame_Info = get_frame_info(page_directory,v,&ptr_page_table);
+		struct FrameInfo *allocc;
+		int ret2 = allocate_frame(&allocc);
+		if(ret2 == E_NO_MEM)
+			return -1 ;
+		int ret3 = 	map_frame(page_directory , allocc , v , perms );
+
+		if( ret3 == E_NO_MEM ){
+			free_frame(allocc);
+			return -1;
+		}
+		v = v + PAGE_SIZE;
+	}
+	return 0;
 }
 
 /*BONUS*/
